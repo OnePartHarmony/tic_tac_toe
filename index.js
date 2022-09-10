bc0109fa400fc532f138566c5d92c7ce8c5b3f91
OnePartHarmony <bakingharmony@gmail.com>
2022-09-10 14:44:18 -0400

    Finished required game, started adding scores

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const board = document.getElementById("board")
-const turn = document.getElementById("turn")
-const box1 = document.getElementById("box1").innerText
-const box2 = document.getElementById("box2").innerText
-const box3 = document.getElementById("box3").innerText
-const box4 = document.getElementById("box4").innerText
-const box5 = document.getElementById("box5").innerText
-const box6 = document.getElementById("box6").innerText
-const box7 = document.getElementById("box7").innerText
-const box8 = document.getElementById("box8").innerText
-const box9 = document.getElementById("box9").innerText
-
 const allBoxes = document.querySelectorAll(".box")
+const turn = document.getElementById("turn")
+let wins = []
 
-//win conditions
-const row1Win = (box1 === box2 && box1 === box3) && (box1 == "X" || box1 == "O")
-const row2Win = (box4 === box5 && box4 === box6) && (box4 == "X" || box4 == "O")
-const row3Win = (box7 === box8 && box7 === box9) && (box7 == "X" || box7 == "O")
-const collumn1Win = (box1 === box4 && box1 === box7) && (box1 == "X" || box1 == "O")
-const collumn2Win = (box2 === box5 && box2 === box8) && (box2 == "X" || box2 == "O")
-const collumn3Win = (box3 === box6 && box3 === box9) && (box3 == "X" || box3 == "O")
-const diagonal1Win = (box1 === box5 && box1 === box9) && (box1 == "X" || box1 == "O")
-const diagonal2Win = (box3 === box5 && box3 === box7) && (box3 == "X" || box3 == "O")
+const winConditions = () => {
+    let box1 = document.getElementById("box1").innerText
+    let box2 = document.getElementById("box2").innerText
+    let box3 = document.getElementById("box3").innerText
+    let box4 = document.getElementById("box4").innerText
+    let box5 = document.getElementById("box5").innerText
+    let box6 = document.getElementById("box6").innerText
+    let box7 = document.getElementById("box7").innerText
+    let box8 = document.getElementById("box8").innerText
+    let box9 = document.getElementById("box9").innerText
 
-const someWin = (row1Win || row2Win || row3Win || collumn1Win || collumn2Win || collumn3Win || diagonal1Win || diagonal2Win)
+    let row1Win = (box1 === box2 && box1 === box3) && (box1 == "X" || box1 == "O")
+    let row2Win = (box4 === box5 && box4 === box6) && (box4 == "X" || box4 == "O")
+    let row3Win = (box7 === box8 && box7 === box9) && (box7 == "X" || box7 == "O")
+    let collumn1Win = (box1 === box4 && box1 === box7) && (box1 == "X" || box1 == "O")
+    let collumn2Win = (box2 === box5 && box2 === box8) && (box2 == "X" || box2 == "O")
+    let collumn3Win = (box3 === box6 && box3 === box9) && (box3 == "X" || box3 == "O")
+    let diagonal1Win = (box1 === box5 && box1 === box9) && (box1 == "X" || box1 == "O")
+    let diagonal2Win = (box3 === box5 && box3 === box7) && (box3 == "X" || box3 == "O")
 
+    let someWin = (row1Win || row2Win || row3Win || collumn1Win || collumn2Win || collumn3Win || diagonal1Win || diagonal2Win)
+    return someWin
+}
 
-// turns alternate x and o, and a tie is called if all boxes fill with no win.
 let playerTurn = "X"
 
 const decidePlayer = () => {
@@ -37,9 +37,15 @@ const decidePlayer = () => {
         } else if (allBoxes[i].innerText == "O") {
             boxesWithO++
         }
-    } console.log(boxesWithO + boxesWithX, someWin)
-        if (boxesWithO + boxesWithX == 9 && someWin == false){
+    }
+        if (boxesWithO + boxesWithX == 9){
             turn.innerText = "It's a tie!"
+        } else if (winConditions() == true) {
+            for (i = 0; i < allBoxes.length; i++) {
+                allBoxes[1].removeEventListener("click", clickBox)
+            }
+                turn.innerText = `${playerTurn} Wins!`
+                wins += playerTurn
         } else if (boxesWithX > boxesWithO) {
             playerTurn = "O"
             turn.innerText = "O's Turn"
@@ -49,8 +55,6 @@ const decidePlayer = () => {
         }
 }
 
-
-// Depending on player, clicking a box will mark x or o
 function clickBox() { 
     if (turn.innerText == "X's Turn") {
         this.innerText = "X"
@@ -60,18 +64,12 @@ function clickBox() {
     decidePlayer()
 }
 
-
-
-
-//reset button clears board and makes boxes clickable
 const clickable = () => {
     for (i = 0; i < allBoxes.length; i++) {
         allBoxes[i].addEventListener("click", clickBox, {once: true})
     }
 }
 clickable()
-
-
 
 const clearBoard = () => {
     for (i = 0; i < allBoxes.length; i++) {
@@ -81,5 +79,4 @@ const clearBoard = () => {
     clickable()
 }
 document.getElementById("reset").addEventListener("click", clearBoard)
-
 
